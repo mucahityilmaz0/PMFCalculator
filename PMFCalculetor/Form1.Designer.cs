5b7c974e90ae2d8ad037959f92293d1af6a4de52
mucahityilmaz <390925@ogr.ktu.edu.tr>
2024-03-03 17:39:47 +0300

    calculator

---
+++ PMFCalculetor/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace PMFCalculetor
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            chooseImage = new Button();
+            pictureBox1 = new PictureBox();
+            PMFText = new TextBox();
+            calculate = new Button();
+            chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            drawChart = new Button();
+            refresh = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)chart1).BeginInit();
+            SuspendLayout();
+            // 
+            // chooseImage
+            // 
+            chooseImage.Location = new Point(70, 8);
+            chooseImage.Name = "chooseImage";
+            chooseImage.Size = new Size(138, 76);
+            chooseImage.TabIndex = 0;
+            chooseImage.Text = "Choose Image";
+            chooseImage.UseVisualStyleBackColor = true;
+            chooseImage.Click += chooseImage_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(12, 105);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(256, 256);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // PMFText
+            // 
+            PMFText.Location = new Point(330, 105);
+            PMFText.Multiline = true;
+            PMFText.Name = "PMFText";
+            PMFText.Size = new Size(107, 427);
+            PMFText.TabIndex = 2;
+            // 
+            // calculate
+            // 
+            calculate.Location = new Point(303, 8);
+            calculate.Name = "calculate";
+            calculate.Size = new Size(134, 76);
+            calculate.TabIndex = 3;
+            calculate.Text = "Calculate";
+            calculate.UseVisualStyleBackColor = true;
+            calculate.Click += calculate_Click;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            chart1.Legends.Add(legend1);
+            chart1.Location = new Point(471, 105);
+            chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            chart1.Series.Add(series1);
+            chart1.Size = new Size(661, 427);
+            chart1.TabIndex = 4;
+            chart1.Text = "chart1";
+            // 
+            // drawChart
+            // 
+            drawChart.Location = new Point(510, 8);
+            drawChart.Name = "drawChart";
+            drawChart.Size = new Size(175, 76);
+            drawChart.TabIndex = 5;
+            drawChart.Text = "Draw Chart";
+            drawChart.UseVisualStyleBackColor = true;
+            drawChart.Click += drawChart_Click;
+            // 
+            // refresh
+            // 
+            refresh.Location = new Point(945, 8);
+            refresh.Name = "refresh";
+            refresh.Size = new Size(167, 76);
+            refresh.TabIndex = 6;
+            refresh.Text = "Refresh";
+            refresh.UseVisualStyleBackColor = true;
+            refresh.Click += refresh_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1157, 544);
+            Controls.Add(refresh);
+            Controls.Add(drawChart);
+            Controls.Add(chart1);
+            Controls.Add(calculate);
+            Controls.Add(PMFText);
+            Controls.Add(pictureBox1);
+            Controls.Add(chooseImage);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)chart1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button chooseImage;
+        private PictureBox pictureBox1;
+        private TextBox PMFText;
+        private Button calculate;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private Button drawChart;
+        private Button refresh;
+    }
+}
---
+++ PMFCalculetor/Form1.cs
@@ -0,0 +1,103 @@
+using System;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace PMFCalculetor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string selectedImagePath;
+
+        private void chooseImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files(*.jpg; *.jpeg; *.png; *.bmp)|*.jpg; *.jpeg; *.png; *.bmp";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                selectedImagePath = openFileDialog.FileName;
+                pictureBox1.Image = Image.FromFile(selectedImagePath);
+            }
+        }
+        double[] pmf = new double[256];
+
+        private void calculate_Click(object sender, EventArgs e)
+        {
+            using (Mat image = CvInvoke.Imread(selectedImagePath, ImreadModes.Grayscale))
+            {
+                Image<Gray, byte> grayImage = image.ToImage<Gray, byte>();
+
+                DenseHistogram histogram = new DenseHistogram(256, new RangeF(0, 256));
+
+                histogram.Calculate(new Image<Gray, byte>[] { grayImage }, false, null);
+
+                float[] histogramValues = new float[256];
+                histogram.CopyTo(histogramValues);
+
+                double totalPixels = grayImage.Width * grayImage.Height;
+
+                for (int i = 0; i < 256; i++)
+                {
+                    pmf[i] = histogramValues[i] / totalPixels;
+
+                }
+                string pmfText = "\r\n";
+                foreach (var value in pmf)
+                {
+                    pmfText += value.ToString("F6") + "\r\n";
+                }
+
+                PMFText.Text = pmfText;
+
+            }
+        }
+
+        private void drawChart_Click(object sender, EventArgs e)
+        {
+
+            int[] X = new int[256];
+            double[] uf = new double[256];
+            for (int i = 0; i <= 255; i++)
+            {
+                X[i] = i;
+                uf[i] = pmf[i];
+            }
+
+            for (int i = 0; i < X.Length; i++)
+            {
+                chart1.Series["Series1"].Points.AddXY(X[i], uf[i]);
+                if (i > 0)
+                {
+                    chart1.Series["Series1"].Points.AddXY(X[i], 0);
+                    chart1.Series["Series1"].Points.AddXY(X[i], uf[i]);
+                }
+            }
+
+            chart1.ChartAreas[0].AxisX.Title = "X kesin uzayi";
+            chart1.ChartAreas[0].AxisY.Title = "Uyelik Dereceleri";
+            chart1.Series["Series1"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart1.Series["Series1"].Color = Color.Blue;
+        }
+
+        private void refresh_Click(object sender, EventArgs e)
+        {
+            chart1.Series.Clear();
+            chart1.ChartAreas.Clear();
+            PMFText.Clear();
+            pictureBox1.Image = null;
+            chart1.Series.Add("Series1");
+            chart1.ChartAreas.Add("ChartAreas");
+
+        }
+    }
+}
